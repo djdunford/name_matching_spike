dba6389525c6bab805830a66906a4db2a8847663
Darren Dunford <48522654+darrendunford@users.noreply.github.com>
2023-10-24 15:17:34 +0100

    refactored in to separate component for name entry

--- name-match/src/App.tsx
+++ name-match/src/App.tsx
@@ -10,31 +10,50 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-
-function InputBoxes() {
-
-  let [value, setValue] = useState("");
-  let [submittedValue, setSubmittedValue] = useState("");
-
-  const onClick = () => {
-    setSubmittedValue(value)
-  }
+function GivenAndFamilyNames({componentName, setValue}: any) {
 
   const onValueChange = (e: any) => {
     setValue(e.target.value);
   }
 
   return (
-    <Box component="form" sx={{
+    <Box component="form" border="3px solid" sx={{
       '& .MuiTextField-root': {m: 1, width: '25ch'},
     }}>
+      <Grid item>
+        <h3>{componentName}</h3>
+      </Grid>
       <Grid container spacing={2}>
         <Grid item>
-          <TextField fullWidth label={"CIC Given Names"} onChange={onValueChange} autoComplete={"off"}/>
+          <TextField fullWidth label={"Given Names"} onChange={onValueChange} autoComplete={"off"}/>
         </Grid>
         <Grid item>
-          <TextField fullWidth label={"CIC Family Names"} autoComplete={"off"}/>
+          <TextField fullWidth label={"Family Names"} autoComplete={"off"}/>
         </Grid>
+      </Grid>
+    </Box>
+  )
+}
+
+function InputBoxes() {
+
+  let [cicValue, setCicValue] = useState("");
+  let [bavValue, setBavValue] = useState("");
+  let [hmrcValue, setHmrcValue] = useState("");
+  let [kbvValue, setKbvValue] = useState("");
+  let [submittedValue, setSubmittedValue] = useState("");
+
+  const onClick = () => {
+    setSubmittedValue(cicValue)
+  }
+
+  return (
+    <Box>
+      <GivenAndFamilyNames componentName={"CIC"} setValue={setCicValue}/>
+      <GivenAndFamilyNames componentName={"BAV"} setValue={setBavValue}/>
+      <GivenAndFamilyNames componentName={"HMRC"} setValue={setHmrcValue}/>
+      <GivenAndFamilyNames componentName={"KBV"} setValue={setKbvValue}/>
+      <Box>
         <Grid item>
           <Button variant="contained" onClick={onClick}>
             Submit
@@ -46,7 +65,7 @@ function InputBoxes() {
             ratio("Fred Flintstone", submittedValue)
           }
         </Grid>
-      </Grid>
+      </Box>
     </Box>
   );
 }
