f08d99d5e3cdfc75d96a03762d01ba5025aa5b26
Darren Dunford <48522654+darrendunford@users.noreply.github.com>
2023-10-24 13:30:49 +0100

    changed to MaterialUI

--- name-match/src/App.tsx
+++ name-match/src/App.tsx
@@ -1,70 +1,64 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import {ratio} from "fuzzball";
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
-const App = (props: any) => {
+
+function InputBoxes(props: any) {
 
   let [value, setValue] = useState("");
   let [updatedValue, setUpdatedValue] = useState("");
 
-  const onFormSubmit = (e: any) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const formDataObject = Object.fromEntries(formData.entries());
-    console.log(formDataObject);
+  const onFormSubmit = () => {
     setUpdatedValue(value)
-  }
-
-  const onValueValueChange = (e: any) => {
-    setValue(e.target.value);
   }
 
   useEffect(() => {
     setValue(props.value)
   }, [props.value]);
 
+  const onValueValueChange = (e: any) => {
+    setValue(e.target.value);
+  }
+
+  return (
+    <Box component="form" sx={{
+      '& .MuiTextField-root': {m: 1, width: '25ch'},
+    }}>
+      <Grid container spacing={2}>
+        <Grid item>
+          <TextField fullWidth label={"CIC Given Names"} onChange={onValueValueChange} autoComplete={"off"}/>
+        </Grid>
+        <Grid item>
+          <TextField fullWidth label={"CIC Family Names"} autoComplete={"off"}/>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" onClick={onFormSubmit}>
+            Submit
+          </Button>
+        </Grid>
+        <Grid item>
+          Fuzzball Ratio:
+          {
+            ratio("Fred Flintstone", updatedValue)
+          }
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <Form onSubmit={onFormSubmit}>
-          <Container fluid>
-            <Row>
-              <Col>
-                <FloatingLabel label="CIC Given Names">
-                  <Form.Control type="text" name="cicGivenNames" onChange={onValueValueChange}/>
-                </FloatingLabel>
-              </Col>
-              <Col>
-                <FloatingLabel label="CIC Family Names">
-                  <Form.Control type="text" name="cicFamilyNames"/>
-                </FloatingLabel>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Button type="submit">
-                  Submit
-                </Button>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                Fuzzball Ratio:
-                {
-                  ratio("Fred Flintstone", updatedValue)
-                }
-              </Col>
-            </Row>
-          </Container>
-        </Form>
-      </header>
+      <InputBoxes/>
     </div>
   );
 }
