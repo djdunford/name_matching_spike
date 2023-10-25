47e9e457a95d02b1b525dc941e2e4e304d0855c9
Darren Dunford <48522654+darrendunford@users.noreply.github.com>
2023-10-25 17:39:25 +0100

    clears value on input field change

--- name-match/src/components/InputBoxes.tsx
+++ name-match/src/components/InputBoxes.tsx
@@ -1,38 +1,49 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
-import {GivenAndFamilyNames} from "./GivenAndFamilyNames";
+import { GivenAndFamilyNames } from "./GivenAndFamilyNames";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
-import {ratio} from "fuzzball";
-import {FullName} from "./FullName";
+import { ratio } from "fuzzball";
+import { FullName } from "./FullName";
 
 export function InputBoxes() {
-
-  let [cicValue, setCicValue] = useState("");
-  let [bavValue, setBavValue] = useState("");
-  let [submittedValue, setSubmittedValue] = useState("");
+  let [claimedName, setClaimedName] = useState("");
+  let [credentialName, setCredentialName] = useState("");
+  let [result, setResult] = useState<number | undefined>(0);
 
   const onClick = () => {
-    console.log("Setting submitted value:", {cicValue});
-    setSubmittedValue(cicValue)
-  }
+    setResult(ratio(credentialName, claimedName));
+  };
+
+  const onChangeClaimedName = (name: string) => {
+    setResult(undefined);
+    return setClaimedName(name);
+  };
+
+  const onChangeCredentialName = (name: string) => {
+    setResult(undefined);
+    return setCredentialName(name);
+  };
+
+  // const onChangedName = (name: string, setName: (name: string) => any) => {
+  //   setResult(undefined);
+  //   return setName(name);
+  // };
 
   return (
     <Box>
-      <GivenAndFamilyNames componentName={"CIC"} setValue={setCicValue}/>
-      <FullName componentName={"BAV"} setValue={setBavValue}/>
+      <GivenAndFamilyNames
+        componentName={"CIC"}
+        setValue={onChangeClaimedName}
+      />
+      <FullName componentName={"BAV"} setValue={onChangeCredentialName} />
       <Box>
         <Grid item>
           <Button variant="contained" onClick={onClick}>
             Submit
           </Button>
         </Grid>
-        <Grid item>
-          Fuzzball Ratio:
-          {
-            ratio("Fred Flintstone", submittedValue)
-          }
-        </Grid>
+        <Grid item>Fuzzball Ratio: {result}</Grid>
       </Box>
     </Box>
   );
